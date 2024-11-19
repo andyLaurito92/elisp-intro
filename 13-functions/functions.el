ef2f778db2f594bdcab15689dcff961a3092f678
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-11-19 23:12:12 +0100

    Starting w/functions

---
+++ 13-functions/functions.el
@@ -0,0 +1,34 @@
+;;; Function -> rule for carrying out a computation given arguments
+;;; A function can return a value, perform a side effect, both or one of these
+
+
+;;; Pure function -> If given the same arguments, returns always the same value and does
+;;; not perform any side effect
+
+;;; In lisp, a function doesn't need to have strictly a name
+;;; It is an object which can optionaly be associated with a symbol
+
+;;; Terms for functions:
+
+;;; 1 - Lambda expressions
+
+;;; 2 - Primitives/built-in functions -> those implemented in C
+
+;;; 3 - Special form -> A primitive that is like afunction, but does not evaluate
+;;; all of its arguments in the usual way. Examples: if, and, while
+
+;;; 4 - Macro -> A construct defined in Lisp. It translates a lisp expression into
+;;; another expression which is to be evaluated instead
+
+;;; 5 - Command -> An object which can be invoked via the command-execute primitive
+;;; usually due to the user typing in a key sequence bound to that command
+
+
+;;; 6 - Closure -> A function object like a lambda, except that it also encloses an
+;;; environment of lexical variable bindings.
+
+;;; 7 - Autoload object -> A placeholder for a real function. If the autoload object is called
+;;; Emacs loads the file and then calls the proper function
+
+
+
