92ba64c595f77a71792c1e4f3b515d4372687744
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-11-20 01:01:37 +0100

    Adding small exercises

--- exercises/playground.el
+++ exercises/playground.el
@@ -22,3 +22,18 @@
 
 (secondplus2 2)
 
+
+(message "hello world")
+
+;;; Find a file with which you are working and move towards its middle. Find its buffer name, file name, length, and your position in the file. 
+
+(buffer-name)
+
+(buffer-file-name)
+
+(buffer-file-length)
+
+(file-size-human-readable
+ (file-attribute-size
+  (file-attributes (buffer-file-name))
+  ) "iec" "" "B")
